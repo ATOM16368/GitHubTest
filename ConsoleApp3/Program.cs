12b3561b1ee8ba78552101d4f1ab9fd7593b4caa
grimm <aleks-yam@mail.ru>
2026-01-03 12:35:09 +0300

    исправлена ошибка и добавлен перехват после выполнения

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github!");
-            ErrorEventArgs;
+            Console.ReadKey();
         }
     }
 }
